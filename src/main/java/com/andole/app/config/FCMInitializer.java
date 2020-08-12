6365757131c89e591f63c2f5a0c3d053eaa24345
andole87 <andole87@gmail.com>
2020-08-12 15:07:58 +0900

    feat(config): FCMInitializer 추가

---
+++ src/main/java/com/andole/app/config/FCMInitializer.java
@@ -0,0 +1,29 @@
+package com.andole.app.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Getter
+@Component
+public class FCMInitializer {
+    @Value("${fcm.account.path}")
+    private String fcmAccountPath;
+
+    @PostConstruct
+    public void initialize() throws IOException {
+        InputStream accountStream = new ClassPathResource(fcmAccountPath).getInputStream();
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(accountStream))
+                .build();
+        FirebaseApp.initializeApp(options);
+    }
+}
