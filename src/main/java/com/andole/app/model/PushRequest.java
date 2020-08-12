43f53ee7b214a6e526a3e1b4c347ff9ee4111fe2
andole87 <andole87@gmail.com>
2020-08-12 15:09:21 +0900

    feat(service): PushService 추가

---
+++ src/main/java/com/andole/app/model/PushRequest.java
@@ -0,0 +1,9 @@
+package com.andole.app.model;
+
+import lombok.Data;
+
+@Data
+public class PushRequest {
+    private String title;
+    private String body;
+}
---
+++ src/main/java/com/andole/app/service/PushService.java
@@ -0,0 +1,45 @@
+package com.andole.app.service;
+
+import com.andole.app.model.PushRequest;
+import com.andole.app.model.Subscription;
+import com.google.firebase.messaging.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PushService {
+    private final SubscribeService subscribeService;
+
+    public void push(PushRequest request) throws FirebaseMessagingException {
+        FirebaseMessaging messaging = FirebaseMessaging.getInstance();
+        WebpushConfig webPush = createWebpush(request);
+
+        List<Message> messages = createMessages(webPush);
+
+        BatchResponse result = messaging.sendAll(messages);
+        log.info("SUCCESS {}, FAILURE {}", result.getSuccessCount(), result.getFailureCount());
+    }
+
+    private WebpushConfig createWebpush(PushRequest request) {
+        return WebpushConfig.builder()
+                .setNotification(new WebpushNotification(request.getTitle(), request.getBody()))
+                .build();
+    }
+
+    private List<Message> createMessages(WebpushConfig webPush) {
+        return subscribeService.findAll().stream()
+                .map(Subscription::getToken)
+                .map(token -> Message.builder()
+                        .setToken(token)
+                        .setWebpushConfig(webPush)
+                        .build())
+                .peek(message -> log.info(message.toString()))
+                .collect(Collectors.toList());
+    }
+}
