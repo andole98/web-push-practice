4a6a55f41d78a526a1b632be1a0215d49341e93c
andole87 <andole87@gmail.com>
2020-08-19 11:31:09 +0900

    initial commit

---
+++ src/main/java/com/andole/app/WebPushPracticeApplication.java
@@ -0,0 +1,11 @@
+package com.andole.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPushPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebPushPracticeApplication.class);
+    }
+}
